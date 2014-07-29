091acf6d6a4a3afa72ae3f386d592168e542487d
sunchen115 <sunchen115work@gmail.com>
2014-07-29 11:23:40 +0800

    optimize code for split hash by key

--- src/split_hash_by_key.rb
+++ src/split_hash_by_key.rb
@@ -5,25 +5,17 @@ end
 
 
 def split_hash_by_key(*param)
-  arr = param.shift
+  slit_hash = param.shift
   split_keys = param
-  if split_keys.size == 1  and  not (arr.has_key? split_keys[0])
+  if split_keys.size == 1  and  not (slit_hash.has_key? split_keys[0])
     raise SplitError.new("hash does not has key #{split_keys[0]}")
   end
-
-  result_arr=[]
-  temp_arr=[]
-  arr.each do|key,value|
-    if split_keys.include? key and not temp_arr.empty?
-        result_arr<< Hash[temp_arr]
-        temp_arr =[[key,value]]
-    else
-      temp_arr<<[key,value]
-    end
-  end
-result_arr << Hash[temp_arr]
+  slit_hash.reduce([{}]) {  |new_arr, item|
+    new_arr<<{} if split_keys.include? item[0];
+    new_arr[-1].merge!(Hash[[item]]);
+    new_arr;
+  }.reject {|item| item.size == 0}
 
 end
-
 
 
