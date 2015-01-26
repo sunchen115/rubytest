36aaf91fabc6bd15f90a7ac78982621fd26fedcf
sunchen115 <sunchen115work@gmail.com>
2015-01-26 09:56:39 +0800

    refactor

--- myexcise/switch_pair/switch.rb
+++ myexcise/switch_pair/switch.rb
@@ -1,8 +1,9 @@
 name_list = %w(sc oy ct fj jp dj)
 
 
-def fetch_random(list, random)
-  index = random % list.size
+def fetch_random(list)
+  random_index = Time.new.strftime("%W").to_i
+  index = random_index % list.size
   res = list.fetch(index)
   list.delete_at(index)
   res
@@ -10,13 +11,10 @@ end
 
 
 pairs = []
-random_index = Time.new.strftime("%W").to_i
 name_list.push('null') if name_list.length.odd?
 ori_list = name_list.sort
 until ori_list.empty?
-  person = ori_list.shift
-  pair_person= fetch_random(ori_list, random_index)
-  pairs<< [person, pair_person]
+  pairs<<[ori_list.shift, fetch_random(ori_list)]
 end
 
 p pairs
